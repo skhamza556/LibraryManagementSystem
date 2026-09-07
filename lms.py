efb53913e9901e7bf035b9f22e4a221f8e57721a
skhamza556 <skhamza554@eng.rizvi.edu.in>
2026-09-07 10:53:31 +0530

    v1.0- Basic Issue and return

---
+++ lms.py
@@ -0,0 +1,6 @@
+# Library Management System - Version 1.0
+# Feature: issue and return books
+def issue_book(book_id, member_id):
+print(&quot;Book&quot;, book_id, &quot;issued to member&quot;, member_id)
+def return_book(book_id):
+print(&quot;Book&quot;, book_id, &quot;returned&quot;)
